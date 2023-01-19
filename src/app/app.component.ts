570b1af7eba3ce430c6a53348cf9b08b8c880416
Rajesh Verma <info.mrverma01@gmail.com>
2023-01-19 22:05:45 +0530

    commite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'random-color';
+
+  public  colorLength : number = 6 ;
+  public color : string = '';
+  public colorChar : string =  '0123456789abcdef';
+  public colorArray : Array<{}> = [];
+
+
+  ngOnInit(){
+    this.loadColor()
+  }
+
+  public createColor() {
+    let newColor = ''
+    for(let i = 0 ; i < this.colorLength; i++){
+      let randomColor = Math.floor(Math.random()* this.colorChar.length)
+       newColor += this.colorChar[randomColor];
+    }
+    this.color = `#${newColor}`;
+    this.colorArray.push(this.color)
+  }
+
+  public loadColor(){
+    let numberOfBox : number = 12; //to create box as much as you want
+    for (let index = 0; index < numberOfBox; index++) {
+      this.createColor()
+    }
+  }
+
+  copyColor(colrName : string | any){
+    navigator.clipboard.writeText(colrName).then(()=>{
+      alert(`${colrName} color copied success`)
+    });
+  }
+
 }
